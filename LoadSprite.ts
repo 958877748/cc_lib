4415293ac9cdcb7e841b7357a53058315b7c3d7d
player1954 <31437616+958877748@users.noreply.github.com>
2025-12-23 17:55:31 +0800

    Refactor LoadSprite class and state management

--- LoadSprite.ts
+++ LoadSprite.ts
@@ -1,112 +1,137 @@
-const { ccclass, property, menu } = cc._decorator
+const { ccclass, menu } = cc._decorator
 
 @ccclass
 @menu('Lib/LoadSprite')
 export default class LoadSprite extends cc.Component {
-    @property(cc.Sprite)
-    sprite: cc.Sprite = null
-    /**
-     * 正在显示的uuid
-     */
-    private uuid_c: string = null
-    /**
-     * 加载中的uuid
-     */
-    private uuid_l: string = null
+    private sprite: cc.Sprite = null
+    private currentState: State
 
     onLoad() {
-        let spriteFrame = this.sprite.spriteFrame
+        this.sprite = this.getComponent(cc.Sprite);
+        const spriteFrame = this.sprite.spriteFrame;
         if (spriteFrame) {
-            this.uuid_c = spriteFrame['_uuid']
+            this.changeState(new DisplayingState(this, spriteFrame));
         } else {
-            this.uuid_c = null
+            this.changeState(new NoSpriteState(this));
         }
     }
-
-    /**
-     * 设置图片路径
-     */
+    onDestroy() {
+        this.currentState.onDestroy()
+    }
+    setSpriteFrame(spriteFrame: cc.SpriteFrame) {
+        this.sprite.spriteFrame = spriteFrame
+    }
+    public getCachedSpriteFrame(url: string): cc.SpriteFrame {
+        const info = cc.resources.getInfoWithPath(url, cc.SpriteFrame)
+        if (!info) {
+            return null;
+        }
+        return cc.assetManager.assets.get(info.uuid) as cc.SpriteFrame
+    }
+    changeState(newState: State): void {
+        if (this.currentState) {
+            this.currentState.onExit()
+            this.currentState.context = null
+        }
+        this.currentState = newState
+        if (this.currentState) {
+            this.currentState.onEnter()
+        }
+    }
     set url(url: string) {
-        if (!url) {
-            //如果传入一个空url,精灵不显示,uuid和uuid加载清空
-            this.sprite.spriteFrame = null
-            this.uuid_c = null
-            this.uuid_l = null
-            return
-        }
-        //默认resources
-        let bundle = cc.resources
-        //检查缓存
-        let info = bundle.getInfoWithPath(url, cc.SpriteFrame)
-        if (info) {
-            //有信息,就在这个resources包里
-            this.showsf(bundle, url, info.uuid)
+        this.currentState.setUrl(url)
+    }
+}
+
+abstract class State {
+    context: LoadSprite
+    constructor(context: LoadSprite) {
+        this.context = context;
+    }
+    onEnter(): void { }
+    onExit(): void { }
+    setUrl(url: string): void { }
+    onDestroy(): void { }
+}
+
+class NoSpriteState extends State {
+    setUrl(url: string): void {
+        let cachedSprite = this.context.getCachedSpriteFrame(url)
+        if (cachedSprite) {
+            this.context.changeState(new DisplayingState(this.context, cachedSprite))
         } else {
-            //resources里没有这个资源
-            //第一个斜杠前是banndle名字
-            //在Load.Bundles里面检查
-            let index = url.indexOf('/')
-            if (index == -1) {
-                throw Error('SpriteData.url ' + url)
-            }
-            //检查是否在load中纪录的banndle
-            let bundle_name = url.slice(0, index)
-            if (!cc.assetManager.bundles.get(bundle_name)) {
-                throw Error('not find bundle ' + bundle_name)
-            }
-            // if (!Load.Bundles.includes(bundle_name)) {
-            //     throw Error('not find bundle ' + bundle_name)
-            // }
-            //检查bundle是否存在
-            let bundle = cc.assetManager.getBundle(bundle_name)
-            if (bundle == null) {
-                throw Error('not find bundle ' + bundle_name)
-            }
-            let bundle_url = url.slice(index + 1)
-            info = bundle.getInfoWithPath(url, cc.SpriteFrame)
-            this.showsf(bundle, bundle_url, info.uuid)
+            this.context.changeState(new LoadingState(this.context, url))
         }
     }
+    onDestroy(): void {
+        this.context.changeState(new DestroyState(this.context));
+    }
+}
 
-    private showsf(bundle: cc.AssetManager.Bundle, url: string, uuid: string) {
-        if (this.uuid_c == uuid) {
-            //当前显示的就是这个,加载中的不再需要
-            this.uuid_l = null
+class DisplayingState extends State {
+    spriteFrame: cc.SpriteFrame
+    constructor(context: LoadSprite, spriteFrame: cc.SpriteFrame) {
+        super(context)
+        this.spriteFrame = spriteFrame
+    }
+    onEnter(): void {
+        this.spriteFrame.addRef();
+        this.context.setSpriteFrame(this.spriteFrame);
+    }
+    onExit(): void {
+        this.spriteFrame.decRef();
+        this.context.setSpriteFrame(null);
+    }
+    setUrl(url: string): void {
+        let cachedSprite = this.context.getCachedSpriteFrame(url)
+        if (cachedSprite) {
+            this.context.changeState(new DisplayingState(this.context, cachedSprite))
         } else {
-            //当前显示的和需要显示的不一样
-            //1.通过uuid取到缓存
-            let asset = cc.assetManager.assets.get(uuid) as cc.SpriteFrame
-            //2.有缓存
-            if (asset) {
-                //使用缓存显示
-                this.sprite.spriteFrame = asset
-                this.uuid_c = uuid
-                this.uuid_l = null
+            this.context.changeState(new LoadingState(this.context, url))
+        }
+    }
+    onDestroy(): void {
+        this.context.changeState(new DestroyState(this.context));
+    }
+}
+
+class LoadingState extends State {
+    private url: string
+    constructor(context: LoadSprite, url: string) {
+        super(context)
+        this.url = url
+    }
+    onEnter(): void {
+        cc.resources.load(this.url, cc.SpriteFrame, (err, asset) => {
+            if (err) {
+                this.onLoadError(err);
             } else {
-                //去加载
-                if (this.uuid_l && this.uuid_l == uuid) {
-                    //如果正在加载且正在加载的uuid和当前传入的uuid一样,啥都不干
-                } else {
-                    //没有正在加载,或者加载的和传入的uuid不一致
-                    this.uuid_l = uuid
-                    bundle.load(url, cc.SpriteFrame, (err, asset) => {
-                        if (err)
-                            console.log(err)
-                        if (asset['_uuid'] == this.uuid_l) {
-                            this.sprite.spriteFrame = asset as cc.SpriteFrame
-                            this.uuid_c = this.uuid_l
-                            this.uuid_l = null
-                        }
-                    })
-                }
+                this.onLoadComplete(asset);
             }
+        })
+    }
+    onLoadError(err: Error) {
+        setTimeout(() => {
+            throw err;
+        })
+        this.context.changeState(new NoSpriteState(this.context))
+    }
+    onLoadComplete(asset: cc.SpriteFrame) {
+        if (this.context) {
+            this.context.changeState(new DisplayingState(this.context, asset))
+        } else {
+            asset.addRef();
+            asset.decRef();
         }
     }
-
-    resetInEditor() {
-        this.sprite = this.getComponent(cc.Sprite)
-        if (this.sprite == null) {
-            this.sprite = this.addComponent(cc.Sprite)
+    onDestroy(): void {
+        this.context.changeState(new DestroyState(this.context));
+    }
+    setUrl(url: string): void {
+        if (this.url !== url) {
+            this.context.changeState(new LoadingState(this.context, url))
         }
     }
 }
+
+class DestroyState extends State { }
